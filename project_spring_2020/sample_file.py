9882410a4f8fed37edd33847c62d9113395f6821
niezu <47363002+niezu@users.noreply.github.com>
2020-04-15 10:13:27 -0400

    Update sample_file.py

--- project_spring_2020/sample_file.py
+++ project_spring_2020/sample_file.py
@@ -1,1 +1,5 @@
-import sys
+# project_spring_2020
+
+[![CircleCI](https://circleci.com/gh/biof309/project_spring_2020/tree/master.svg?style=shield)](https://circleci.com/gh/biof309/project_spring_2020/tree/master)
+
+I would like to learn how to use python program to do the data analysis of the image data. To compare the different effects of Doc to c-Myc and its mutants.  I have all the Excel data file.
